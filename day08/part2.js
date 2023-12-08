2393ee8730cd04bfa4f64c5b36cca527dedae345
Mike Madsen <mikewmadsen@gmail.com>
2023-12-08 06:44:53 -0800

    finished day 8

--- day08/part2.js
+++ day08/part2.js
@@ -22,9 +22,10 @@ for (let point in map) {
   if (point[2] === 'A') curr.push(point);
 }
 console.log(curr);
-let steps = 0;
 let relsteps = 0;
 let done = false
+const tracking = []
+
 while (!done) {
   let dir = dirs[relsteps];
   done = true;
@@ -36,14 +37,70 @@ while (!done) {
       curr[i] = options[1];
     }
     if (curr[i][2] !== 'Z') done = false;
+    if (curr[i][2] === 'Z') {
+      if (!tracking[curr[i]]) tracking[curr[i]] = [steps+1];
+    }
   }
-  // if (relsteps === 0) console.log(curr, steps)
-  steps++; // increment steps
+  if (Object.keys(tracking).length === curr.length) done = true;
+
   relsteps++;
   if (relsteps === dirs.length) relsteps = 0;
 }
 
-console.log(curr)
-console.log(steps);
+let sum = 1;
+
+// i think I have to do prime factorization
+
+const isPrime = (num) => {
+  let i = 2;
+  while (i <= num / 2) {
+    if (num % i === 0) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+}
+
+for (let val in tracking) {
+  let num = tracking[val].shift();
+  // console.log(num)
+  while (!isPrime(num)) {
+    let i = 2;
+    loop:
+    while (i <= num / 2) {
+      if (isPrime(i) && num % i == 0) {
+        tracking[val].push(i);
+        num = num / i;
+        break loop;
+      }
+      i += 1;
+    }
+  }
+  tracking[val].push(num);
+}
+
+const lcms = [];
+
+for (let nums of Object.values(tracking)) {
+  for (num of nums) {
+    if (!lcms.includes(num)) lcms.push(num);
+  }
+}
+
+for (let mults of lcms) {
+  sum *= mults;
+}
+
+
+
+console.log(tracking);
+
+console.log(sum);
 
 // its hanging again
+// tried math approach: 1.7805435973203438e+25
+// that was dumb
+// prime factorization:
+// 8245452805243
+
