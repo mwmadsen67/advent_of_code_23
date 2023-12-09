cda3dd5c4ad43694ab57340ffbdbb7298db32bca
Mike Madsen <mikewmadsen@gmail.com>
2023-12-09 05:21:50 -0800

    finish day 9

---
+++ day09/part2.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const codeInput = fs.readFileSync('day09/puzzleinput.txt', 'utf8').split('\n').map(str => str.split(' ').map(num => parseInt(num)));
+
+// console.log(codeInput)
+// [
+//   [ 0, 3, 6, 9, 12, 15 ],
+//   [ 1, 3, 6, 10, 15, 21 ],
+//   [ 10, 13, 16, 21, 30, 45 ]
+// ]
+
+let ans = [];
+for (let line of codeInput) {
+  let arr = [line];
+  while (arr[arr.length - 1][arr[arr.length -1].length - 1] !== 0) {
+    let lastline = arr[arr.length-1];
+    let eles = [];
+    for (let i = 0; i < lastline.length - 1; i++) {
+      eles.push(lastline[i+1] - lastline[i]);
+    }
+    arr.push(eles);
+  }
+  for (let i = arr.length - 1; i > 0; i--) {
+    let start = arr[i][0];
+    let next = arr[i - 1][0];
+    arr[i - 1].unshift(next - start);
+  }
+  ans.push(arr[0][0]);
+}
+
+// console.log(ans)
+console.log(ans.reduce((acc, cur) => acc + cur));
